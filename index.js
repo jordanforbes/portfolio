8540417a7089ef250756d1ccc2bf9e63341a8aa5
jordanforbes <Jordforb@gmail.com>
2017-05-26 11:05:06 -0400

    added face

---
+++ index.js
@@ -0,0 +1,32 @@
+var bodyparser = require('body-parser')
+var express = require('express')
+
+var pub = __dirname
+
+var app = express()
+
+var portnum = 5000
+
+//setup middleware
+app.use(express.static(pub))
+app.use('/views', express.static(pub + '/views'));
+app.use('/css', express.static(__dirname + '/css'))
+app.use('/assets', express.static(pub + '/assets'))
+
+//view engine
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile)
+
+app.get('/', function(req, res){
+	res.render('index')
+})
+
+app.use(function(err, req, res, next) {
+  res.send(err.stack);
+});
+
+if (!module.parent) {
+  var port = process.env.PORT || portnum;
+  app.listen(port);
+  console.log('Express started on port '+ portnum);
+}
