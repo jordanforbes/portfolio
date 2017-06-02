003169547104e43fc0593f9391c6d08dd0099e17
jordanforbes <Jordforb@gmail.com>
2017-06-02 13:31:19 -0400

    added routes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var app = express()
 
 var portnum = 5000
 
-//setup middleware
+// setup middleware
 app.use(express.static(pub))
 app.use('/views', express.static(pub + '/views'));
 app.use('/css', express.static(__dirname + '/css'))
@@ -17,9 +17,17 @@ app.use('/assets', express.static(pub + '/assets'))
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile)
 
+//index
 app.get('/', function(req, res){
-	res.render('index')
+	res.render('pages/index')
 })
+
+//webapps page
+app.get('/apps', function(req, res){
+	res.render('pages/webapps')
+})
+
+
 
 app.use(function(err, req, res, next) {
   res.send(err.stack);
