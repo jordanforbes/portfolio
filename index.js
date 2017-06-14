52bd8b3996ebff89a77a85abea1b44ee51e3c67a
jordanforbes <Jordforb@gmail.com>
2017-06-14 16:08:27 -0400

    added pages

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var bodyparser = require('body-parser')
 var express = require('express')
 
+var engine = require('ejs-layout');
+
 var pub = __dirname
 
 var app = express()
 
-var portnum = 5000
+var portnum = 9000
 
 // setup middleware
 app.use(express.static(pub))
@@ -15,16 +17,32 @@ app.use('/assets', express.static(pub + '/assets'))
 
 //view engine
 app.set('view engine', 'ejs');
-app.engine('html', require('ejs').renderFile)
+// app.engine('html', require('ejs').renderFile)
+app.engine('ejs', engine.__express)
 
 //index
 app.get('/', function(req, res){
-	res.render('pages/index')
+	res.render('newindex')
 })
 
 //webapps page
 app.get('/apps', function(req, res){
-	res.render('pages/webapps')
+	res.render('pages/apps')
+})
+
+//about
+app.get('/about', function(req, res){
+	res.render('pages/about')
+})
+
+//contact
+app.get('/contact', function(req, res){
+	res.render('pages/contact')
+})
+
+//skills
+app.get('/skills', function(req, res){
+	res.render('pages/skills')
 })
 
 
